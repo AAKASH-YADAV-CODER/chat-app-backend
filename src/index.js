d9616a8e5034d2a305f8998584053428c630dbb0
AAKASH-YADAV-CODER <ay614838@gmail.com>
2024-04-09 14:27:49 +0530

    merging frontend

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import connect from "./database/index.js";
 import cors from "cors";
 import { app, server } from "./socket/socket.js";
 import express from "express";
+import path from "path";
 // const app = express();
 app.use(express.json());
 app.use(cookieParser());
@@ -16,7 +17,7 @@ dotenv.config({
   path: ".env",
 });
 const port = process.env.PORT || 5000;
-
+const __dirname = path.resolve();
 //Route Definitions
 import userRouter from "./routers/user.router.js";
 import messageRouter from "./routers/message.router.js";
@@ -24,6 +25,11 @@ import getuserRouter from "./routers/getuser.router.js";
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/message", messageRouter);
 app.use("/api/v1/getuser", getuserRouter);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(port, () => {
   connect();
